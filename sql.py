8bdfdd8700580dfcfb386d9a7d0bbd2d7e9b250a
thyrusg <thyrus.gorges@gmail.com>
2016-05-09 18:42:23 -0700

    SQL Time
    Added a todo list for the basic SQL related stuff. Schema up next.

---
+++ sql.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+# to do
+# create the database schemea
+# add checking to make sure the schema exists 
+# write functions for adding items to the database
+# make sure the INSERT function checks for existing/duplicate entries
+# write a search function to locate items based upon the schema
