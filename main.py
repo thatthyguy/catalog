642b2298cead6789d359f6e3ead2f4780ce3476d
thyrusg <thyrus.gorges@gmail.com>
2016-05-09 18:04:27 -0700

    More stuff
    Updated README with a short description.
    Threw in some basic code for the main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+import os, sys
+
+# To start, let's just list every file in a directory
+
+directory = str(sys.argv[1])
+print 'Moving to ' + directory
+os.chdir(directory)
+print 'We are in ' + os.getcwd()
