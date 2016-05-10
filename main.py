75911406c9ba51a7e7158ded06beb66737979f77
thyrusg <thyrus.gorges@gmail.com>
2016-05-09 18:10:58 -0700

    Removed some directory commands
    Since we don't actually need to move to a directory to list
    its contents first, I removed those unncessary commands.

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ import os, sys
 # To start, let's just list every file in a directory
 
 directory = str(sys.argv[1])
-print 'Moving to ' + directory
-os.chdir(directory)
-print 'We are in ' + os.getcwd()
 print "Listing files now..."
-
 
 for f in os.listdir(directory):
     print f
