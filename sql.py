cfa4016d55d2a8ebe3612cfdadce33be97d7d38a
thyrusg <thyrus.gorges@gmail.com>
2016-05-09 20:24:46 -0700

    Schema time
    Came up with a super duper basic schema. Will probably change in the future.

--- sql.py
+++ sql.py
@@ -6,3 +6,13 @@ import sqlite3
 # write functions for adding items to the database
 # make sure the INSERT function checks for existing/duplicate entries
 # write a search function to locate items based upon the schema
+
+# schema - just one big ole' table for now
+# table name: pdfs
+# id: primary key int
+# full_path: char 
+# filename: char
+# author: char
+# title: char
+# page length: int
+# publisher (maybe): char
