682ab67c6fd96894630dbcf92bd2d70e7b201343
thyrusg <thyrus.gorges@gmail.com>
2016-05-09 18:09:21 -0700

    Files this time
    Finally added the ability to list the files of a specified directory

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ directory = str(sys.argv[1])
 print 'Moving to ' + directory
 os.chdir(directory)
 print 'We are in ' + os.getcwd()
+print "Listing files now..."
+
+
+for f in os.listdir(directory):
+    print f
